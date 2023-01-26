ab69c1fdb3a0a09ef8a9ed65154726dc4e65cb67
FergusJames212 <fergusarjames@virginmedia.com>
2023-01-26 11:48:33 +0000

    pokemon progress

--- pokemon.js
+++ pokemon.js
@@ -6,6 +6,28 @@ class Pokemon {
         this.attackDamage = attackDamage;
         this.move = move;
     };
-}
+
+    isEffectiveAgainst(opponent) {
+        if (this.type === "water" && opponent.type === "fire") {
+            return true;
+        } else if (this.type === "fire" && opponent.type === "water") {
+            return false;
+        };
+    };
+
+    isWeakTo(opponent) {
+        if (this.type === "water" && opponent.type === "fire") {
+            return false;
+        } else if (this.type === "fire" && opponent.type === "water") {
+            return true;
+        };
+    };
+
+    takeDamage(opponent) {
+        this.hitPoints = this.hitPoints - opponent.attackDamage;
+    };
+};
+
+
 
 module.exports = Pokemon;
